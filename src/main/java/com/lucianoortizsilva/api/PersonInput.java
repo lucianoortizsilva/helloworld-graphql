f8df622156ab8681837484e720edd404b6218cea
ortiz <luciano.ortiz@neogrid.com>
2018-08-20 17:54:50 -0300

    remove lombok

--- src/main/java/com/lucianoortizsilva/api/PersonInput.java
+++ src/main/java/com/lucianoortizsilva/api/PersonInput.java
@@ -2,19 +2,12 @@ package com.lucianoortizsilva.api;
 
 import java.io.Serializable;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 /**
  * 
  * @author lucianoortizsilva@gmail.com
  * @since 2018/08/20
  *
  */
-@Getter
-@Setter
-@ToString
 public class PersonInput implements Serializable {
 
 	private static final long serialVersionUID = 7937454393886012114L;
@@ -22,5 +15,21 @@ public class PersonInput implements Serializable {
 	private String firstname;
 	private String lastname;
 	private Integer age;
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
 
 }
